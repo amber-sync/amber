2efe95349b2608b4599b12f829c4068d9959cf24
florianmahner <florian.mahner@icloud.com>
2026-01-28 21:42:58 +0100

    Remove unused analytics and explorer components

--- src/components/analytics/index.ts
+++ src/components/analytics/index.ts
@@ -1,4 +1,1 @@
 export { BackupCalendar } from './BackupCalendar';
-export { StorageProjection } from './StorageProjection';
-export { FileTypeBreakdown } from './FileTypeBreakdown';
-export { BackupHealth } from './BackupHealth';
